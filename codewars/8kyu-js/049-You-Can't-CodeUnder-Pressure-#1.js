6c74b2eb2af70cc491fdf758ae2900884f276421
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-10-28 18:44:48 +0100

    Resolved problem 049-You-Can't-CodeUnder-Pressure-#1

---
+++ codewars/8kyu-js/049-You-Can't-CodeUnder-Pressure-#1.js
@@ -0,0 +1,9 @@
+/* 
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+console.log("******Solucion 1********");
+// i will be an integer. Double it and return it.
+const doubleInteger = (i) => i * 2;
+
+console.log(doubleInteger(2));
