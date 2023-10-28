ea8454547b7e9a826b3df41eed49384e3ac50ef1
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-10-28 16:06:41 +0100

    Resolved problem 043-MakeUpperCase

---
+++ codewars/8kyu-js/043-MakeUpperCase.js
@@ -0,0 +1,8 @@
+/* 
+Write a function which converts the input string to uppercase.
+*/
+
+console.log("******Solucion 1********");
+const makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase("hola mundo"));
