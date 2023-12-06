da87b55e71d6d36424ab92fad748d07af877928f
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-12-06 12:01:26 +0000

    Resolved problem 063-Sum-Mixed-Array

--- codewars/8kyu-js/063-Sum-Mixed-Array.js
+++ codewars/8kyu-js/063-Sum-Mixed-Array.js
@@ -25,7 +25,8 @@ console.log(sumMix2([9, 3, "7", "3"])); //22
 console.log(sumMix2(["5", "0", 9, 3, 2, 1, "9", 6, 7])); //42
 console.log(sumMix2(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"])); //41
 
-// !"operador unario más" +n
+/* 
+! "operador unario más" +n */
 /* el operador + cuando se utiliza para convertir un valor a número se llama "operador de conversión a número" o . Este operador unario más realiza la conversión de tipo a número en JavaScript.
 
 Se usa principalmente cuando deseas asegurarte de que un valor sea tratado como un número. Aquí hay algunos casos comunes en los que se utiliza: */
