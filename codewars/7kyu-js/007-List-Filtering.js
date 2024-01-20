b1a30fa80b8e4f0b1f856b8229fc1bdb527ed18b
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-01-20 19:04:06 +0000

    Resolved problem 007-List-Filtering

---
+++ codewars/7kyu-js/007-List-Filtering.js
@@ -0,0 +1,24 @@
+/* 
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+ */
+
+console.log("******Solution 1********");
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter((element) => Number.isInteger(element));
+}
+
+console.log(filter_list([1, 2, "a", "b"]));
+
+console.log("******Solution 2********");
+function filter_list2(l) {
+  // Return a new array with the strings filtered out
+  return l.filter((element) => typeof element === "number");
+}
+
+console.log(filter_list2([1, 2, "a", "b"]));
